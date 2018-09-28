89b56e69d59b2f7546456e2e681ec18535da0032
Adonis Gaitatzis <backupbrain@gmail.com>
2018-09-27 20:39:17 -0700

    adding setup script

--- camera/setup.sh
+++ camera/setup.sh
@@ -3,10 +3,10 @@ wget https://bootstrap.pypa.io/get-pip.py
 python get-pip.py
 wget "https://github.com/jabelone/OpenCV-for-Pi/raw/master/latest-OpenCV.deb"
 apt-get update
-apt-get upgrade
-apt-get install build-essential cmake
-apt-get install libgtk-3-dev
-apt-get install libboost-all-dev
+apt-get upgrade -y
+apt-get install -y build-essential cmake
+apt-get install -y libgtk-3-dev
+apt-get install -y libboost-all-dev
 
 pip install numpy
 pip install scipy
@@ -14,12 +14,12 @@ pip install scikit-image
 pip install picamera
 pip install pathlib2
 
-apt-get install libtiff5-dev libjasper-dev libpng12-dev
-apt-get install libjpeg-dev
-apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
-apt-get install libgtk2.0-dev
-apt-get install libatlas-base-dev gfortran
-apt-get install python-dev
+apt-get install -y libtiff5-dev libjasper-dev libpng12-dev
+apt-get install -y libjpeg-dev
+apt-get install -y libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+apt-get install -y libgtk2.0-dev
+apt-get install -y libatlas-base-dev gfortran
+apt-get install -y python-dev
 
 pip install numpy
 
