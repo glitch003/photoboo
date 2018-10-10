0c1f064606b9bc9779f6a93885fb33b22da5a25a
Adonis Gaitatzis <backupbrain@gmail.com>
2018-10-09 20:21:52 -0700

    adding pushbutton support

--- camera/photoboo/PhotoBooManager.py
+++ camera/photoboo/PhotoBooManager.py
@@ -42,7 +42,8 @@ class PhotoBooManager(object):
                 images_folder.as_posix()
             ))
             raise SystemExit
-
+        # give camera time to warm up
+        time.sleep(2)
         camera.capture(tmp_image_filepath.as_posix())
         camera.close()
         return tmp_image_filepath.as_posix()
