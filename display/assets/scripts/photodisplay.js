1be9dd9b87a568f93a340305ea60f133d8e1e939
Adonis Gaitatzis <backupbrain@gmail.com>
2018-10-10 22:12:42 -0700

    updating display modes

--- display/assets/scripts/photodisplay.js
+++ display/assets/scripts/photodisplay.js
@@ -1,3 +1,141 @@
 function PhotoDisplay() {
-	
+	this.apiUrl = 'https://20mission.org/photoboo/api/photos/';
+	this.lastPhotoCtime = 0;
+	this.timeBetweenPhotos_s = 5;
+}
+
+PhotoDisplay.prototype.getRandomPhoto = function() {
+	self = this;
+	url = this.apiUrl + "random";
+	$.ajax({
+		dataType: "json",
+		url: url,
+		success: function(request) {
+			self.hideError();
+			self.displayPhoto(request);
+			console.log(request);
+		},
+		error: function(error) {
+			self.displayError("Could not contact API server");
+			console.log(error);
+		}
+	});
+}
+
+PhotoDisplay.prototype.playRandomPhotos = function() {
+	self = this;
+	self.getRandomPhoto();
+	setInterval(function() {
+		self.getRandomPhoto();
+	}, this.timeBetweenPhotos_s * 1000);
+}
+
+PhotoDisplay.prototype.playLatestPhoto = function() {
+	self = this;
+	this.getLatestPhoto();
+	setInterval(function() {
+		self.getLatestPhoto();
+	}, this.timeBetweenPhotos_s * 1000);
+}
+
+PhotoDisplay.prototype.getPhotoById = function(id) {
+	self = this;
+	url = this.apiUrl + id;
+	$.ajax({
+		dataType: "json",
+		url: url,
+		success: function(request) {
+			self.hideError();
+			self.displayPhoto(request);
+			console.log(request);
+		},
+		error: function(error) {
+			self.displayError("Could not contact API server");
+			console.log(error);
+		}
+	});
+}
+
+PhotoDisplay.prototype.listPhotos = function() {
+	self = this;
+	url = this.apiUrl;
+	$.ajax({
+		dataType: "json",
+		url: url,
+		success: function(request) {
+			self.hideError();
+			console.log(request);
+		},
+		error: function(error) {
+			self.displayError("Could not contact API server");
+			console.log(error);
+		}
+	});
+}
+
+PhotoDisplay.prototype.getLatestPhoto = function() {
+	self = this;
+	url = this.apiUrl;
+	lastPhotoCtime = this.lastPhotoCtime;
+	$.ajax({
+		dataType: "json",
+		url: url,
+		success: function(request) {
+			self.hideError();
+			localMaxCtime = 0;
+			localMaxCtimeId = 0;
+			for (var i = 0; i < request.length; i++) {
+				photo = request[i];
+				if (photo.ctime > localMaxCtime) {
+					self.lastPhotoCtime = photo.ctime
+					self.getPhotoById(photo.id);
+					break;
+				}
+				console.log(photo);
+			}
+		},
+		error: function(error) {
+			self.displayError("Could not contact API server");
+			console.log(error);
+		}
+	});
+}
+
+PhotoDisplay.prototype.displayPhoto = function(photo) {
+	/*
+	console.log("Displaying photo:");
+	loadingPhoto = $("#loading_photo");
+	loadedPhoto = $("#loaded_photo");
+	loadingPhoto.css({ opacity: 0.0 });
+	data = photo["data"];
+	background = 'url(data:image/jpg;base64,' + data + ')'; 
+	console.log(data.substring(0, 10));
+	console.log(background.substring(0, 100));
+	loadingPhoto.css(
+		"background-image",
+		background
+	);
+	loadingPhoto.fadeTo("slow", 1.0, function() {
+		loadedPhoto.css(
+			"background-image",
+			background
+		);
+		loadingPhoto.css({opacity: 0.0});
+  	});
+  	*/
+	$("#photo").attr(
+		"src",
+		"data:image/jpg;base64," + photo.data
+	);
+	/* */
+}
+
+PhotoDisplay.prototype.displayError = function(message) {
+	console.log("ERROR: " + message);
+	$("#message_bar").html(message);
+	$("#message_bar").slideDown();
+}
+
+PhotoDisplay.prototype.hideError = function() {
+	$("#message_bar").slideUp();
 }
