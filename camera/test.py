6c67dac3872d72bd650e613702462f9c5752ec54
Chris Cassano <1285652+glitch003@users.noreply.github.com>
2019-10-21 04:10:37 -0700

    test image

--- camera/test.py
+++ camera/test.py
@@ -2,6 +2,7 @@ from photoboo.PhotoBooManager import PhotoBooManager
 import time
 
 image_filepath = "beatles.jpg"
+# image_filepath = "original_1571629730.jpg"
 # image_filepath = "22829408_10155695942842660_4876641366014296389_o.jpg"
 # image_filepath = "adonis-moustache.jpg"
 
