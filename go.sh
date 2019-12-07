6aeefefe312e4c3e1e14bc30f1432c22b6b4f8b5
Chris Cassano <1285652+glitch003@users.noreply.github.com>
2019-12-06 16:29:47 -0800

    also make go log

--- go.sh
+++ go.sh
@@ -3,5 +3,5 @@
 while :
 do
   # loop forever in case this gets quit somehow
-  xterm -maximize -e "python3 /home/pi/camera/chris_gui_main_run_this.py"
+  xterm -maximize -e "python3 /home/pi/camera/chris_gui_main_run_this.py >> /home/pi/log.txt"
 done
